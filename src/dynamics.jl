436b0a0e3e4a233d83b6d04fd4e9c15ddbad9a0e
bbhattacharyya1729 <bbhattac@caltech.edu>
2025-03-21 19:19:06 -0500

    Fixed Adjoint

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -21,16 +21,21 @@ using TestItemRunner
 function dynamics_components(integrators::Vector{<:AbstractIntegrator})
     dynamics_comps = []
     comp_mark = 0
+    # for integrator ∈ integrators
+    #     if(!(typeof(integrator)<:AdjointBilinearIntegrator))
+    #         integrator_comps = comp_mark .+ (1:integrator.x_dim)
+    #         push!(dynamics_comps, integrator_comps)
+    #         comp_mark += integrator.x_dim
+    #     else 
+    #         integrator_comps = comp_mark .+ (1:integrator.x_dim+integrator.xₐ_dim)
+    #         push!(dynamics_comps, integrator_comps)
+    #         comp_mark += integrator.x_dim + integrator.xₐ_dim
+    #     end
+    # end
     for integrator ∈ integrators
-        if(!(typeof(integrator)<:AdjointBilinearIntegrator))
-            integrator_comps = comp_mark .+ (1:integrator.x_dim)
-            push!(dynamics_comps, integrator_comps)
-            comp_mark += integrator.x_dim
-        else 
-            integrator_comps = comp_mark .+ (1:integrator.x_dim+integrator.xₐ_dim)
-            push!(dynamics_comps, integrator_comps)
-            comp_mark += integrator.x_dim + integrator.xₐ_dim
-        end
+        integrator_comps = comp_mark .+ (1:integrator.x_dim)
+        push!(dynamics_comps, integrator_comps)
+        comp_mark += integrator.x_dim
     end
     return dynamics_comps
 end
@@ -118,7 +123,9 @@ struct TrajectoryDynamics
             println("        constructing full dynamics derivative functions...")
         end
 
-        dynamics_dim = sum(typeof(integrator)<:AdjointBilinearIntegrator ? integrator.x_dim + integrator.xₐ_dim : integrator.x_dim  for integrator ∈ integrators) 
+        #dynamics_dim = sum(typeof(integrator)<:AdjointBilinearIntegrator ? integrator.x_dim + integrator.xₐ_dim : integrator.x_dim  for integrator ∈ integrators) 
+        dynamics_dim = sum(integrator.x_dim  for integrator ∈ integrators) 
+
         dynamics_comps = dynamics_components(integrators)
 
         @views function F!(
--- src/integrators/adjoint_bilinear_integrator.jl
+++ src/integrators/adjoint_bilinear_integrator.jl
@@ -6,12 +6,10 @@ using ExponentialAction
 struct AdjointBilinearIntegrator <: AbstractIntegrator
     G::Function
     x_comps::AbstractVector{Int}
-    xₐ_comps::AbstractVector{Int}
     u_comps::AbstractVector{Int}
     Δt_comp::Int
     z_dim::Int
     x_dim::Int
-    xₐ_dim::Int
     u_dim::Int
     
 
@@ -27,13 +25,11 @@ struct AdjointBilinearIntegrator <: AbstractIntegrator
 
         return new(
             G,
-            traj.components[x],
-            traj.components[xₐ],
+            vcat(traj.components[xₐ],traj.components[x]),
             traj.components[u],
             traj.components[traj.timestep][1],
             traj.dim,
-            traj.dims[x],
-            traj.dims[xₐ],
+            traj.dims[xₐ]+traj.dims[x],
             traj.dims[u]
         )
     end
@@ -47,13 +43,11 @@ end
     xₖ₊₁ = zₖ₊₁[B.x_comps]
     xₖ = zₖ[B.x_comps]
 
-    xₐₖ₊₁ = zₖ₊₁[B.xₐ_comps]
-    xₐₖ = zₖ[B.xₐ_comps]
-    
     uₖ = zₖ[B.u_comps]
     Δtₖ = zₖ[B.Δt_comp]
-    δₖ[:] = vcat(xₐₖ₊₁,xₖ₊₁) - expv(Δtₖ, B.G(uₖ), vcat(xₐₖ,xₖ))
+    δₖ[:] = xₖ₊₁ - expv(Δtₖ, B.G(uₖ), xₖ)
 end
+
 
 @views function jacobian!(
     ∂f::AbstractMatrix,
@@ -64,7 +58,7 @@ end
     ForwardDiff.jacobian!(
         ∂f,
         (δ, zz) -> B!(δ, zz[1:B!.z_dim], zz[B!.z_dim+1:end]),
-        zeros(B!.x_dim+B!.xₐ_dim),
+        zeros(B!.x_dim),
         [zₖ; zₖ₊₁]
     )
 end
@@ -73,34 +67,25 @@ function jacobian_structure(B::AdjointBilinearIntegrator)
 
     z_dim = B.z_dim
     x_dim = B.x_dim
-    xₐ_dim = B.x_dim
     u_dim = B.u_dim
-    
 
     x_comps = B.x_comps
-    xₐ_comps = B.xₐ_comps
     u_comps = B.u_comps
     Δt_comp = B.Δt_comp
 
-    ∂f = spzeros(x_dim+xₐ_dim, 2 * z_dim)
+    ∂f = spzeros(x_dim, 2 * z_dim)
 
     # ∂xₖ₊₁f
-    ∂f[1:x_dim, z_dim .+ x_comps] = I(x_dim)
+    ∂f[:, z_dim .+ x_comps] = I(x_dim)
 
     # ∂xₖf
-    ∂f[1:x_dim, x_comps] = ones(x_dim, x_dim)
-
-    # ∂xₖ₊₁f
-    ∂f[x_dim+1:x_dim+xₐ_dim, z_dim .+ xₐ_comps] = I(xₐ_dim)
-
-    # ∂xₖf
-    ∂f[x_dim+1:x_dim+xₐ_dim,  xₐ_comps] = ones(xₐ_dim, xₐ_dim)
+    ∂f[:, x_comps] = ones(x_dim, x_dim)
 
     # ∂uₖf
-    ∂f[:, u_comps] = ones(x_dim+xₐ_dim, u_dim)
+    ∂f[:, u_comps] = ones(x_dim, u_dim)
 
     # ∂Δtₖf
-    ∂f[:, Δt_comp] = ones(x_dim+xₐ_dim)
+    ∂f[:, Δt_comp] = ones(x_dim)
 
     return ∂f
 end
@@ -114,7 +99,7 @@ end
 )
     return ForwardDiff.hessian(
         zz -> begin
-            δ = zeros(eltype(zz), B!.x_dim+B!.xₐ_dim)
+            δ = zeros(eltype(zz), B!.x_dim)
             B!(δ, zz[1:B!.z_dim], zz[B!.z_dim+1:end])
             return μₖ'δ
         end,
@@ -125,12 +110,10 @@ end
 function hessian_structure(B::AdjointBilinearIntegrator)
 
     x_comps = B.x_comps
-    xₐ_comps = B.xₐ_comps
     u_comps = B.u_comps
     Δt_comp = B.Δt_comp
 
     x_dim = B.x_dim
-    xₐ_dim = B.xₐ_dim
     u_dim = B.u_dim
 
     μ∂²f = spzeros(2 * B.z_dim, 2 * B.z_dim)
@@ -140,12 +123,6 @@ function hessian_structure(B::AdjointBilinearIntegrator)
 
     # μ∂ₓₖ∂Δtₖf & μ∂Δtₖ∂ₓₖf
     μ∂²f[x_comps, Δt_comp] = ones(x_dim)
-
-    # μ∂ₓₖ∂ᵤf & μ∂ᵤ∂ₓₖf
-    μ∂²f[xₐ_comps, u_comps] = ones(xₐ_dim, u_dim)
-
-    # μ∂ₓₖ∂Δtₖf & μ∂Δtₖ∂ₓₖf
-    μ∂²f[xₐ_comps, Δt_comp] = ones(xₐ_dim)
 
     # μ∂u∂Δtₖf & μ∂Δtₖ∂uf
     μ∂²f[u_comps, Δt_comp] = ones(u_dim)
@@ -158,4 +135,3 @@ function hessian_structure(B::AdjointBilinearIntegrator)
 
     return μ∂²f
 end
-
