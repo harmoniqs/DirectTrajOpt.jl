3961e3fa94a0c361b4d0803ecc2cccfa332ba670
Gennadi Ryan <gennadiryan@gmail.com>
2026-04-01 18:11:41 -0600

    Fixing MadNLP.jl tests

--- src/solvers/madnlp_solver/solver.jl
+++ src/solvers/madnlp_solver/solver.jl
@@ -8,10 +8,11 @@ using Libdl  # Added for Pardiso library loading
 
 import IpoptSolverExt: IpoptEvaluator
 
+export _solve
 export solve!
 
 
-function _solve_madnlp!(
+function _solve(
     prob::DirectTrajOptProblem,
     options::MadNLPOptions = MadNLPOptions();
     verbose::Bool = true,
@@ -262,6 +263,55 @@ end
         constraints = AbstractConstraint[g_u_norm],
     )
 
-    MadNLPSolverExt._solve_madnlp!(prob; max_iter = 100)
+    solve!(prob; options=MadNLPSolverExt.MadNLPOptions(max_iter=100))
 end
+
+@testitem "testing MadNLP.jl solver with NonlinearGlobalKnotPointConstraint" begin
+
+    include("../../../test/test_utils.jl")
+
+    G, traj = bilinear_dynamics_and_trajectory(add_global = true)
+
+    integrators = [
+        BilinearIntegrator(G, :x, :u, traj),
+        DerivativeIntegrator(:u, :du, traj),
+        DerivativeIntegrator(:du, :ddu, traj),
+    ]
+
+    J = TerminalObjective(x -> norm(x - traj.goal.x)^2, :x, traj)
+    J += QuadraticRegularizer(:u, traj, 1.0)
+    J += QuadraticRegularizer(:du, traj, 1.0)
+    J += MinimumTimeObjective(traj)
+
+    # Add global objective - minimize global parameter
+    J += GlobalObjective(g -> norm(g)^2, :g, traj; Q = 1.0)
+
+    # Knot point constraint with global dependency
+    # Couples control magnitude with global parameter
+    g_ug = NonlinearGlobalKnotPointConstraint(
+        ug -> begin
+            u = ug[1:traj.dims[:u]]
+            g = ug[(traj.dims[:u]+1):end]
+            return [norm(u) * (1.0 + norm(g)) - 2.0]
+        end,
+        [:u],
+        [:g],
+        traj;
+        times = 2:(traj.N-1),
+        equality = false,
+    )
+
+    prob =
+        DirectTrajOptProblem(traj, J, integrators; constraints = AbstractConstraint[g_ug])
+
+    solve!(prob; options=MadNLPSolverExt.MadNLPOptions(max_iter=100))
+
+    # Verify constraint is satisfied at each timestep
+    for k = 2:(traj.N-1)
+        u = traj[k][:u]
+        g = traj.global_data[traj.global_components[:g]]
+        @test norm(u) * (1.0 + norm(g)) <= 2.0 + 1e-6
+    end
+end
+
 
