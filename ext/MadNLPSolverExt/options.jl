70fe464642a5d2184cff495f6e8dfdd5fd949a86
Gennadi Ryan <gennadiryan@gmail.com>
2026-04-13 18:55:21 -0400

    Implementing full suite of MadNLP-specific options

--- ext/MadNLPSolverExt/options.jl
+++ ext/MadNLPSolverExt/options.jl
@@ -7,79 +7,107 @@ export MadNLPOptions
     print_level::Int = 3 # corresponds to MadNLP.LogLevels(print_level) where TRACE = 1 <= print_level <= 6 = ERROR
     eval_hessian::Bool = true
 
-    # ##
+    ##
 
-    # # Primary options
-    # tol::T = 1e-8
+    # Primary options
+    tol::T = 1e-8
+    callback::Symbol # [:sparse, :dense]
+    kkt_system::Symbol # [:unreduced, :reduced, :condensed]
+    linear_solver::Symbol # [:lapack, :ldl, :mumps]
 
-    # # TODO: add remaining primary, general, and output options
+    # TODO: add general, and output options
 
-    # # Termination options
-    # acceptable_tol::T = 1e-6
-    # acceptable_iter::Int = 15
-    # diverging_iterates_tol::T = 1e20
-    # max_iter::Int = 3000
-    # max_wall_time::T = 1e6
-    # s_max::T = 100.
+    # Termination options
+    acceptable_tol::T = 1e-6
+    acceptable_iter::Int = 15
+    diverging_iterates_tol::T = 1e20
+    max_iter::Int = 3000
+    max_wall_time::T = 1e6
+    s_max::T = 100.
 
-    # # NLP options
-    # kappa_d::T = 1e-5
-    # # fixed_variable_treatment::Symbol = :make_parameter # [:make_parameter, :relax_bound] # TODO: use given defaults as defaults UNLESS `kkt_system` type parameter is a MadNLP.SparseCondensedKKTSystem
-    # # equality_treatment::Symbol = :enforce_equality # [:enforce_equality, :relax_equality] # TODO: use given defaults as defaults UNLESS `kkt_system` type parameter is a MadNLP.SparseCondensedKKTSystem
-    # bound_relax_factor::T = 1e-8
-    # jacobian_constant::Bool = false
-    # hessian_constant::Bool = false
-    # # hessian_approximation::Symbol = :exact_hessian # # TODO
-    # # quasi_newton_options::Nothing = nothing # # TODO: at minimum, support setting BGFSInitStrategy
-    # # inertia_correction_method::Symbol = :inertia_auto # TODO: what options are there?
-    # # inertia_free_tol::T = 0. # TODO: should this default remain fixed given an alternative choice of `inertia_correction_method`?
-    # default_primal_regularization::T = 0. # Recently added option of indeterminate utility; see https://github.com/MadNLP/MadNLP.jl/pull/573
-    # default_dual_regularization::T = 0. # Recently added option of indeterminate utility; see https://github.com/MadNLP/MadNLP.jl/pull/573
+    # NLP options
+    kappa_d::T = 1e-5
+    # fixed_variable_treatment::Symbol = :make_parameter # [:make_parameter, :relax_bound] # Determines how `MadNLP.AbstractCallback` reparameterizes the NLP, together with `equality_treatment`; the given defaults are preferred, unless `kkt_system isa MadNLP.SparseCondensedKKTSystem`
+    fixed_variable_treatment::Symbol = ((callback == :sparse && kkt_system == :condensed) ? :relax_bound : :make_parameter)
+    # equality_treatment::Symbol = :enforce_equality # [:enforce_equality, :relax_equality] # Determines how `MadNLP.AbstractCallback` reparameterizes the NLP, together with `fixed_variable_treatment`; the given defaults are preferred, unless `kkt_system isa MadNLP.SparseCondensedKKTSystem`
+    equality_treatment::Symbol = ((callback == :sparse && kkt_system == :condensed) ? :relax_equality : :enforce_equality)
+    bound_relax_factor::T = 1e-8
+    jacobian_constant::Bool = false
+    hessian_constant::Bool = false
+    hessian_approximation::Symbol = :exact_hessian # [:exact_hessian, :bfgs, :damped_bfgs, :compact_lbfgs] # Note that the options :bfgs and :damped_bfgs are incompatible with sparse KKT systems/sparse linear solvers
+    inertia_correction_method::Symbol = :inertia_auto # [:inertia_auto, :inertia_based, :inertia_ignore, :inertia_free] # Mechanism for regularizing reduced Hessian such that its inertia is (n_primal, 0, n_dual); if not using an "inertia-revealing" solver, this defaults to :inertia_free; see https://github.com/MadNLP/MadNLP.jl/blob/master/src/IPM/inertiacorrector.jl
+    inertia_free_tol::T = 0. # Only applicable when `inertia_correction_method == :inertia_free`; see https://github.com/MadNLP/MadNLP.jl/blob/master/src/IPM/solver.jl
+    default_primal_regularization::T = 0. # Recently added option of indeterminate utility; has no analog in Ipopt; see https://github.com/MadNLP/MadNLP.jl/pull/573
+    default_dual_regularization::T = 0. # Recently added option of indeterminate utility; has no analog in Ipopt; see https://github.com/MadNLP/MadNLP.jl/pull/573
 
-    # # initialization options
-    # dual_initialized::Bool = false
-    # # dual_initialization_method::Symbol = :zero # [:zero, :least_squares] # TODO: ?
-    # constr_mult_init_max::T = 1e3
-    # bound_push::T = 1e-2 # (corresponds to kappa_1)
-    # bound_fac::T = 1e-2 # should be "bound_frac" (corresponds to kappa_2)
-    # nlp_scaling::Bool = true # (corresponds to [:gradient => true, :none => false], with no support for :user, :equilibration)
-    # nlp_scaling_max_gradient::T = 100.
+    quasi_newton_init_strategy::Symbol = :scalar1 # [:scalar_1, :scalar_2, :scalar_3, :scalar_4, :constant]
+    quasi_newton_max_history::Int = 6
+    quasi_newton_init_value::T = 1.0
+    quasi_newton_sigma_min::T = 1e-8
+    quasi_newton_sigma_max::T = 1e+8   
 
-    # # Hessian Perturbation
-    # min_hessian_perturbation::T = 1e-20
-    # first_hessian_perturbation::T = 1e-4
-    # max_hessian_perturbation::T = 1e20
-    # perturb_inc_fact_first::T = 1e2
-    # perturb_inc_fact::T = 8.
-    # perturb_dec_fact::T = 1/3
-    # jacobian_regularization_exponent::T = 1/4
-    # jacobian_regularization_value::T = 1e-8
+    # Initialization options
+    dual_initialized::Bool = false
+    # dual_initialization_method::Symbol = :least_squares # [:least_squares, :set_to_zero]
+    dual_initialization_method::Symbol = ((callback == :sparse && kkt_system == :condensed) ? :set_to_zero : :least_squares)
+    constr_mult_init_max::T = 1e3
+    bound_push::T = 1e-2 # (corresponds to kappa_1)
+    bound_fac::T = 1e-2 # should be "bound_frac" (corresponds to kappa_2)
+    nlp_scaling::Bool = true # (corresponds to [:gradient => true, :none => false], with no support for :user, :equilibration)
+    nlp_scaling_max_gradient::T = 100.
 
-    # # restoration options
-    # soft_resto_pderror_reduction_factor::T = 0.9999
-    # required_infeasibility_reduction::T = 0.9
+    # Hessian Perturbation
+    min_hessian_perturbation::T = 1e-20
+    first_hessian_perturbation::T = 1e-4
+    max_hessian_perturbation::T = 1e20
+    perturb_inc_fact_first::T = 1e2
+    perturb_inc_fact::T = 8.
+    perturb_dec_fact::T = 1/3
+    jacobian_regularization_exponent::T = 1/4
+    jacobian_regularization_value::T = 1e-8
 
-    # # Line search
-    # # obj_max_inc::T = 5.
-    # # kappha_soc::T = 0.99
-    # max_soc::Int = 4
-    # alpha_min_frac::T = 0.05
-    # s_theta::T = 1.1
-    # s_phi::T = 2.3
-    # eta_phi::T = 1e-4 # Armijo relaxation factor; Ipopt uses default of 1e-8
-    # kappa_soc::T = 0.99
-    # gamma_theta::T = 1e-5 # Constraint violation filter margin relaxation factor
-    # gamma_phi::T = 1e-5 # Barrier fn filter margin relaxation factor; Ipopt uses default of 1e-8
-    # delta::T = 1 # Switching rule constraint violation multiplier
-    # kappa_sigma::T = 1e10 # primal estimate-dual variable deviation limitation factor
-    # barrier_tol_factor::T = 10. # Barrier stop test mu factor
-    # rho::T = 1000. # Restoration penalty parameter (corresponds to IpoptOptions.resto_penalty_parameter, NOT IpoptOptions.rho)
+    # Restoration options
+    soft_resto_pderror_reduction_factor::T = 0.9999 # soft restoration phase primal-dual error required reduction
+    required_infeasibility_reduction::T = 0.9 # restoration phase termination criterion
 
-    # # Barrier
-    # # mu_min by courtesy of Ipopt
-    # # barrier::AbstractBarrierUpdate{T} = MonotoneUpdate(T(tol), barrier_tol_factor) # supports MonotoneUpdate, and QualityFunctionUpdate <: AbstractAdaptiveUpdate, LOQOUpdate <: AbstractAdaptiveUpdate, each of which have further parameterizations.
-    # # barrier::Symbol = :monotone # [:monotone, :adaptive] # TODO: add associated parameters common to all barrier update schemes as well as those particular to each adaptive scheme
-    # tau_min::T = 0.99 # Lower bound on fraction to boundary
+    # Line search
+    obj_max_inc::T = 5. # max acceptable order of magnitude increase in barrier objective
+    kappa_soc::T = 0.99 # 2nd order correction sufficient reduction rule factor
+    max_soc::Int = 4 # 2nd order correction max trial steps per iteration
+    alpha_min_frac::T = 0.05 # minimal step size safety factor
+    s_theta::T = 1.1 # switching rule current constraint violation exponent
+    s_phi::T = 2.3 # switching rule linear barrrier function exponent
+    eta_phi::T = 1e-4 # Armijo relaxation factor; Ipopt uses default of 1e-8
+    gamma_theta::T = 1e-5 # constraint violation filter margin relaxation factor
+    gamma_phi::T = 1e-5 # barrier function filter margin relaxation factor; Ipopt uses default of 1e-8
+    delta::T = 1. # switching rule constraint violation multiplier
+    kappa_sigma::T = 1e10 # primal estimate-dual variable deviation limitation factor
+    barrier_tol_factor::T = 10. # barrier stop test mu factor
+    rho::T = 1000. # restoration penalty parameter (corresponds to IpoptOptions.resto_penalty_parameter, NOT IpoptOptions.rho)
+    
+    # kappha_soc::T = 0.99 # typo of `kappa_soc`
+
+    # Barrier
+    barrier::Symbol = :monotone # [:monotone, :adaptive] # barrier update algorithm
+    tau_min::T = 0.99 # lower bound on fraction to boundary
+
+    barrier_update_mu_init::T = 1e-1
+    barrier_update_mu_min::T = 1e-11
+    barrier_update_mu_superlinear_decrease_power::T = 1.5
+    barrier_update_mu_linear_decrease_factor::T = .2
+
+    barrier_update_mu_max::T = 1e5
+    barrier_update_sigma_min::T = 1e-6
+    barrier_update_sigma_max::T = 1e2
+    barrier_update_sigma_tol::T = 1e-2
+    barrier_update_gamma::T = 1.0
+    barrier_update_max_gs_iter::Int = 8
+    barrier_update_free_mode::Bool = true
+    barrier_update_globalization::Bool = true
+    barrier_update_n_update::Int = 0
+
+    barrier_update_gamma::T = 0.1
+    barrier_update_r::T = .95
 
 end
 
