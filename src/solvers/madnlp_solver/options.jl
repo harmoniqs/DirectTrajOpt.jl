29c8c191f3ededc7e4ea66d10ca93a4e9ae2eed8
Gennadi Ryan <gennadiryan@gmail.com>
2026-04-19 19:36:00 -0400

    Making MadNLPOptions accessible from the main DTO module

---
+++ src/solvers/madnlp_solver/options.jl
@@ -0,0 +1,13 @@
+export MadNLPOptionsStub
+
+@kwdef mutable struct MadNLPOptionsStub <: Solvers.AbstractSolverOptions
+    # Primary options
+    tol::Float64 = 1e-8
+    max_iter::Int = 3000
+    print_level::Int = 3 # (MadNLP.TRACE::MadNLP.LogLevels = 1, ..., MadNLP.ERROR::MadNLP.LogLevels = 6)
+    hessian_approximation::String = "exact" # (exact = MadNLP.ExactHessian, compact_lbfgs = MadNLP.CompactLBFGS) # no other QN methods supported in conjunction with MadNLP.SparseCallback
+
+    # # Only supported by DirectTrajOpt._solve, as an optional kwarg override of `hessian_approximation`;
+    # #   `hessian_approximation = eval_hessian ? "exact" : "compact_lbfgs"`
+    # eval_hessian::Bool = true
+end
