83da0761265257f600586c693f11a9810c77b378
Jack Champagne <jackchampagne.r@gmail.com>
2026-04-24 16:53:14 -0400

    Revert "Merge pull request #67 from harmoniqs/benchmarks/directtrajopt-initial"
    This reverts commit 19fa68ad630ccf30141aee73ac4aba2a9207dd1a, reversing
    changes made to 9239b1a1b2834c0ac69cb2e97f90f39dd78b3eb2.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using DirectTrajOpt
 using TestItemRunner
 
 
-# Exclude benchmark/ testitems — those run in a separate project environment
-@run_package_tests filter=ti -> !contains(ti.filename, "benchmark")
+# Run all testitem tests in package
+@run_package_tests
